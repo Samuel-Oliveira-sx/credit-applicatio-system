33fb6686615049e4da0e02203774dacb90e98e9e
Samuel Oliveira <samuel.sax@outlook.com>
2024-01-23 18:14:12 -0300

    arquivos commit

---
+++ src/main/kotlin/me/dio/credit/application/system/entity/Adress.kt
@@ -0,0 +1,12 @@
+package me.dio.credit.application.system.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Adress (
+    @Column(nullable = false) var zipCode: String = "",
+    @Column(nullable = false) var street: String = ""
+)
+
+
